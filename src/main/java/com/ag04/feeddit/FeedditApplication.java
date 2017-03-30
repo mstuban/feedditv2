55a5e7a015dad05f6c9d288b322fe7388faa86cf
mstuban <stuban.marko@gmail.com>
2017-03-30 13:19:38 +0200

    Changed from spring boot starter thymeleaf to a separate Thymeleaf Configuration file. Added new TemplateResolver and resource handlers.

--- src/main/java/com/ag04/feeddit/FeedditApplication.java
+++ src/main/java/com/ag04/feeddit/FeedditApplication.java
@@ -1,40 +1,13 @@
 package com.ag04.feeddit;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.view.ResourceBundleViewResolver;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
-import org.thymeleaf.templateresolver.TemplateResolution;
-import org.thymeleaf.templateresolver.TemplateResolver;
 
 @SpringBootApplication
 public class FeedditApplication {
 
-    @Autowired
-    private ThymeleafProperties properties;
-
-    @Value("${spring.thymeleaf.templates_root:}")
-    private String templatesRoot;
-
     public static void main(String[] args) {
         SpringApplication.run(FeedditApplication.class, args);
-    }
-
-    @Bean
-    public ITemplateResolver templateResolver() {
-        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
-        resolver.setSuffix(properties.getSuffix());
-        resolver.setPrefix(templatesRoot);
-        resolver.setTemplateMode(properties.getMode());
-        resolver.setCacheable(properties.isCache());
-        return resolver;
     }
 
 }
